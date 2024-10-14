e9ac149214f7b821c41fc6c82a738195848f10c0
alebernini <alebernini@gmail.com>
2024-10-14 20:21:33 +0100

    Feedback updated

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ import plotly.express as px
 import streamlit as st
 
 car_data = pd.read_csv('vehicles_us.csv') # lendo os dados
-hist_button = st.button('Criar histograma') # criar um botão
 
+hist_button = st.button('Criar histograma') # criar um botão
 if hist_button: # se o botão for clicado
     # escrever uma mensagem
     st.write('Criando um histograma para o conjunto de dados de anúncios de vendas de carros')
@@ -13,7 +13,8 @@ if hist_button: # se o botão for clicado
     fig = px.histogram(car_data, x="odometer")
 
     # exibir um gráfico Plotly interativo
-    st.plotly_chart(fig, use_container_width=True)        
+    st.plotly_chart(fig, use_container_width=True)
+            
 
 disp_button = st.button('Criar gráfico de dispersão') # criar um botão
 if disp_button: # se o botão for clicado
